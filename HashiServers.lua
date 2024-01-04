f6226476ca7ad184a38467ee727842d2f82af16e
Dimsk <87251198+DimskOfficial@users.noreply.github.com>
2024-01-04 09:27:44 +0700

    Update HashiServers.lua

--- HashiServers.lua
+++ HashiServers.lua
@@ -1,3 +1,4 @@
+print("Запускаю HashiServers.lua")
 local term = require("term")
   term.clear()
 
@@ -9,12 +10,11 @@ local text = [[
  |_| |_|\__,_|___/_| |_|_|____/ \___|_|    \_/ \___|_|  |___/
                                                               ]]
 
--- Очистка консоли
-clearConsole()
-
 -- Вывод текста
 print(text)
-print("Бесплатная нагрузка для серверов")
+print("Завершить программу")
+print("(CTRL + ALT + C)")
+print(" ")
 print("Version 1.2")
 print("Created by Dimsk and ChatGPT")
 print("Подгружаю библиотеки..")
@@ -22,10 +22,10 @@ local computer = require("computer")
 local shell = require("shell")
 
 local url = "https://github.com/DimskOfficial/kafifs/raw/main/saturn.pic"
-local installPath = "/path/to/saturn.pic"  -- Укажите полный путь к файлу на вашем компьютере
-local repetitions = 5  -- Количество повторений загрузки
-local interval = 0.1  -- Интервал в секундах между загрузками
-local duration = 1  -- Продолжительность в секундах перед перезагрузкой
+local installPath = "/path/to/saturn.pic"  
+local repetitions = 5 
+local interval = 0.1  
+local duration = 1  
 os.sleep(0.3)
 print("Загружено")
 
@@ -33,12 +33,12 @@ print("Создаю директорию для файла")
 shell.execute("mkdir -p '/path/to'")
 print("Создал директорию")
 
--- Функция для загрузки файла по указанному URL через wget с принудительной перезаписью
+
 local function downloadFile(url, path)
   shell.execute("wget -f " .. url .. " '" .. path .. "'")
 end
 
--- Функция для установки изображения указанное количество раз
+
 local function installImage()
   for i = 1, repetitions do
     downloadFile(url, installPath)
@@ -46,13 +46,13 @@ local function installImage()
   end
 end
 
--- Запускаем функцию для установки изображения
+
 installImage()
 print("Подготавливаюсь к перезагрузке")
 os.sleep(0.2)
 print("Перезагружаю..")
 
--- Ждем указанное количество секунд перед перезагрузкой
+
 os.sleep(duration)
 
 
